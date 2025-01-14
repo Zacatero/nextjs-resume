1c6b90dec56096ffd0b53ec13c07550d5d5c7872
Max <max@zacate.ro>
2025-01-14 00:35:41 -0500

    Update

--- edit-me/config/links.ts
+++ edit-me/config/links.ts
@@ -9,28 +9,13 @@ import {
 
 export const links: CMSLink[] = [
   {
-    href: 'https://www.linkedin.com',
+    href: 'https://www.linkedin.com/in/max-trivers/',
     icon: SiLinkedin,
     title: 'LinkedIn',
   },
   {
-    href: 'https://github.com',
+    href: "https://github.com/Zacatero',
     icon: SiGithub,
     title: 'GitHub',
-  },
-  {
-    href: 'https://www.npmjs.com',
-    icon: SiNpm,
-    title: 'NPM',
-  },
-  {
-    href: 'https://bsky.app',
-    icon: SiBluesky,
-    title: 'Bluesky',
-  },
-  {
-    href: 'https://www.instagram.com',
-    icon: SiInstagram,
-    title: 'Instagram',
   },
 ];
